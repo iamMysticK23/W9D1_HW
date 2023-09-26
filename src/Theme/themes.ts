b300134e33403b82e0dccb5814e501a8afc82432
Kenai Epps <k.epps23@gmail.com>
2023-09-25 21:51:41 -0700

    first commit

---
+++ src/Theme/themes.ts
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material'; 
+
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'
+    },
+    palette: {
+        primary: {
+            main: '#eb8e21'
+        },
+        secondary: {
+            main: '#ffa901'
+        },
+        info : {
+            main: '#363b78'
+        }
+    }
+})
---
+++ src/components/Shop/Shop.tsx
@@ -0,0 +1,16 @@
+import * as React  from 'react'; 
+
+// internal imports
+import { NavBar } from '../sharedComponents';
+
+
+
+
+export const Shop = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>Shop For A Car</h1>
+        </div>
+    )
+}
